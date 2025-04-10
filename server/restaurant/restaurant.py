98b3b01a671cad2769bb241616332ac7fdf8bdda
ivan lambev <ivan2007ny@gmail.com>
2025-04-10 23:25:15 +0300

    updated items get request

--- server/restaurant/restaurant.py
+++ server/restaurant/restaurant.py
@@ -297,7 +297,7 @@ async def add_items(
 #     ).all()
 #     return rows
 
-@app.get("/restaurants/items{restaurant_id}")
+@app.get("/restaurants/{restaurant_id}/items")
 async def get_items(
     restaurant_id: UUID,  # This will be parsed from the URL path
     db=Depends(get_db_session),
